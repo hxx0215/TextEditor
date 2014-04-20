2702114a62e99ae14c6328417090b5d85b6fd9d3
hxx0215 <hxx0215@126.com>
2014-04-20 10:41:20 +0800

    Initial Commit

---
+++ TextEditor/KDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KDAppDelegate.h
+//  TextEditor
+//
+//  Created by hxx on 14-4-20.
+//  Copyright (c) 2014年 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextEditor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TextEditor
+//
+//  Created by hxx on 14-4-20.
+//  Copyright (c) 2014年 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KDAppDelegate class]));
+    }
+}
